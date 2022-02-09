eb40d6e35260d19d4a0af2be671b558bf7b1838f
Szymon Dziedzic <sz.dziedz@gmail.com>
2022-02-10 00:47:04 +0100

    set up react navigation

--- example/App.tsx
+++ example/App.tsx
@@ -1,63 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
-import exampleMapStyle from './exampleMapStyle.json';
-
-import * as Maps from 'expo-maps';
+import { NavigationContainer } from '@react-navigation/native';
+import MainNavigator from './navigators/MainNavigator';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Maps.ExpoMap
-        style={{ flex: 1, width: '100%' }}
-        provider="apple"
-        googleMapsJsonStyleString={JSON.stringify(exampleMapStyle)}
-      >
-        <Maps.Marker latitude={-33.86} longitude={151.2} />
-        <Maps.Marker latitude={-32} longitude={152} />
-        <Maps.Polygon
-          points={[
-            {
-              latitude: -30,
-              longitude: 150,
-            },
-            {
-              latitude: -29,
-              longitude: 152,
-            },
-            {
-              latitude: -31,
-              longitude: 150,
-            },
-          ]}
-        />
-        <Maps.Polyline
-          points={[
-            {
-              latitude: -29,
-              longitude: 150,
-            },
-            {
-              latitude: -29,
-              longitude: 151,
-            },
-            {
-              latitude: -28,
-              longitude: 152,
-            },
-          ]}
-        />
-      </Maps.ExpoMap>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <MainNavigator />
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
